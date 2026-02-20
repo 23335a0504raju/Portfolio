bf64f8777efd3acd1a81e3b672207cbbb3b9ea53
rajuspotmies <raju@spotmies.ai>
2026-02-20 12:30:21 +0530

    feat: add new Contact section component with interactive cards, animations, and background effects

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -41,7 +41,7 @@ const Contact = () => {
     {
       label: 'Download CV',
       icon: <FaFileDownload size={28} />,
-      link: 'https://drive.google.com/file/d/1jlWoA1ELx0KIQFRUt5AHsjI6Hf1kSi5d/view?usp=drive_link',
+      link: 'https://drive.google.com/file/d/12-iWWjxQ1jO14D3cwAnLJALMgThYX1l_/view?usp=drive_link',
       description: 'Grab my latest resume.',
       image: resumeBg
     }
